ceef2c66018b91380e5e83cf45db82f13225fa88
chrmay502 <chrmay502@gmail.com>
2024-09-20 12:51:17 -0700

    Replaced single quotes with backticks on line 6 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component ({
   standalone: true,
   selector: 'app-root',
-  template: '
+  template: `
     <main>
       <header class ="brand-name">
         <img class="brand-logo" src="/assets/logo.svg"
         alt="logo" aria-hidden="true">
 </header>
 </main>
-',
-  styleUrls: ['./app.component.css'],
+`, styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
